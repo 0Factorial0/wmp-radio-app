6e13a4d9c0daf7b74d0b466899aa819d69f7b9f9
0Factorial0 <work.mehill@gmail.com>
2026-09-10 11:32:55 +0300

    auto push

---
+++ main.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+
+try:
+    import webview
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pywebview"])
+    import webview
+if __name__ == '__main__':
+    webview.create_window(
+        'WMP Battery Radio',
+        'radio.html',
+        width=250,
+        height=300,
+        resizable=False,
+        min_size=(250, 300),
+        frameless=True,
+        easy_drag=True,
+        background_color='#0b0b12'
+    )
+    webview.start()
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Simulating Old Windows Media Player Animations & Connecting It Into A Radio Syncer // Desktop Widget
+
+1. use start.bat to start the widget or make a shortcut
+2. close the terminal when you want to close the app
+
+free to use
+
+//0x0
